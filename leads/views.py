c353ff447ea4a834cc73642bfed487f1a86de0c5
Syed Ahsan Raza <75121164+syedahsanraza1@users.noreply.github.com>
2023-09-10 01:22:04 +0500

    working with views
    working with views created template folders

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home_page(request):
+    #return HttpResponse("Hellow Worlld")
+    return render(request, 'next_page.html')
