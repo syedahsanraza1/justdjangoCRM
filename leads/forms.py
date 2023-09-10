12db30c1a48b98d9a81311cfbb7fadfeaea74c78
Syed Ahsan Raza <75121164+syedahsanraza1@users.noreply.github.com>
2023-09-10 16:04:34 +0500

    updating form
    creating an update lead form

--- leads/forms.py
+++ leads/forms.py
@@ -1,4 +1,15 @@
 from django import forms
+from .models import leads
+
+class leadmodelform(forms.ModelForm):
+    class Meta:
+        model  = leads
+        fields = (
+            'first_name',
+            'last_name',
+            'age',
+            'agent',
+        )
 
 class leadform(forms.Form):
     first_name = forms.CharField()
